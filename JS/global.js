ff6410a763c2412d265491dba8d6669e3df1660c
Riyaz Ahmed <24riyazahmedaug2004@gmail.com>
2024-01-25 23:47:12 +0530

    Updated Styles

--- JS/global.js
+++ JS/global.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 /*
@@ -12,18 +11,3 @@
 
 
 
-=======
-
-
-/*
-* @param {NodeList} $elements
-* @param {String} eventType Event type string
-* @param {Function} callback Callback function 
-*/
-
-
-
-
-
-
->>>>>>> 884f40011442cff6e6c295432837fb8104d08d7f
