97581c2c35e31c70c6d546b7232a4ed9f28a5fb2
Riyaz Ahmed <153071174+RiaAug24@users.noreply.github.com>
2024-01-25 23:36:31 +0530

    "Fixed layout issues"

--- JS/global.js
+++ JS/global.js
@@ -1,29 +1,13 @@
-"use strict";
+
 
 /*
- * @param {Array} queries Query array
- * @param {Function} successCallback successCallback function
- */
+* @param {NodeList} $elements
+* @param {String} eventType Event type string
+* @param {Function} callback Callback function 
+*/
 
-window.ACCESS_POINT = " https://api.edamam.com/api/recipes/v2";
-const APP_ID = "1d56952c";
-const API_KEY = "1381bfc880a44ab80149dd1489b90f1e";
-const TYPE = "public";
 
-export const fetchData = async function (queries, successCallback) {
-  const query = queries
-    ?.join("&")
-    .replace(/,/g, "=")
-    .replace(/g /g, "%20")
-    .replace(/\+/g, "%2B");
 
-  const url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${API_KEY}&type=${TYPE}${query ? `&${query}` : ""}`;
 
-  const response = await fetch(url);
 
-  if (response.ok) {
-    const data = await response.json();
-    successCallback(data);
-  }
 
-};
