1b48267bec67d220f7dbaf18494e35c2a457854e
Riyaz Ahmed <24riyazahmedaug2004@gmail.com>
2024-01-26 21:27:36 +0530

    Added new home-tab features

--- JS/home.js
+++ JS/home.js
@@ -1,3 +1,33 @@
+"use strict";
+
+/**
+ * API Setup
+ */
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+
+const APP_ID = "1d56952c";
+const API_KEY = "1381bfc880a44ab80149dd1489b90f1e";
+const TYPE = "public";
+const fetchData = async function (queries, successCallBack) {
+  const query = queries
+    ?.join("&")
+    .replace(/,/g, "=")
+    .replace(/ /g, "%20")
+    .replace(/\+/g, "%2B");
+
+  const url = `${ACCESS_POINT}?app_id=${APP_ID}&app_key=${API_KEY}&type=${TYPE}${
+    query ? `&${query}` : ""
+  }`;
+
+  const response = await fetch(url);
+
+  if (response.ok) {
+    const data = await response.json();
+    successCallBack(data);
+  }
+};
+
 /**
  * home page search
  */
@@ -51,6 +81,100 @@ addEventOnElements($tabBtns, "click", function () {
 
   $lastActiveTabPanel = $currentTabPanel;
   $lastActiveTabBtn = this;
+
+  addTabContent(this, $currentTabPanel);
 });
 
+const cardQueries = [
+  ["field", "uri"],
+  ["field", "label"],
+  ["field", "image"],
+  ["field", "totalTime"],
+];
 
+const $skeletonCard = `
+   <li class="card skeleton-card">
+     <div class="skeleton card-banner"></div>
+
+      <div class="card-body">
+        <div class="skeleton card-title"></div>
+        <div class="skeleton card-text"></div>
+      </div>
+  </li>`;
+
+const addTabContent = ($currentTabBtn, $currentTabPanel) => {
+  const $gridList = document.createElement("div");
+
+  $gridList.classList.add("grid-list");
+
+  $currentTabPanel.innerHTML = `
+  <ul class="container grid-list">
+  ${$skeletonCard.repeat(12)}
+  </div>
+  `;
+
+  fetchData(
+    [
+      ["mealType", $currentTabBtn.textContent.trim().toLowerCase()], ...cardQueries],
+    
+      function (data) {
+      //console.log(data);
+
+      $currentTabPanel.innerHTML = "";
+
+      for (let i = 0; i < 12; i++) {
+        const {
+          recipe: { 
+            image, 
+            label: title, 
+            totalTime: cookingTime, 
+            uri},
+        } = data.hits[i];
+
+        const $card = document.createElement("div");
+        $card.classList.add("card");
+        $card.style.animationDelay = `${100 * i}ms`;
+
+        $card.innerHTML = `
+        <figure class="card-media img-holder">
+          <img src="${image}" loading="lazy" class="img-cover" alt="${title}" />
+        </figure>
+
+        <div class="card-body">
+          <h3 class="title-small">
+            <a href="detail.html" class="card-link">${title ?? "Untitled"}</a>
+          </h3>
+          <div class="meta-wrapper">
+            <div class="meta-item">
+              <span aria-hidden="true">
+                <i class="fa-regular fa-clock"></i></span>
+              <span class="label-medium" style="margin-left: 0.25rem">
+                ${cookingTime || "< 1"} minutes</span>
+            </div>
+            <button class="icon-btn has-state removed" aria-label="Add to saved recipes">
+              <span class="bookmark-add" aria-hidden="true">
+                <i class="fa-regular fa-bookmark"></i>
+              </span>
+
+              <span class="bookmark-remove" aria-hidden="true">
+                <i class="fa-regular fa-trash-can"></i>
+              </span>
+            </button>
+          </div>
+        </div>
+        `;
+
+        $gridList.appendChild($card);
+      }
+
+      $currentTabPanel.appendChild($gridList);
+
+      $currentTabPanel.innerHTML += `
+
+      <a href="recipe.html" class="btn btn-secondary label-large has-state">Show more</a>
+      `;
+
+    });
+};
+
+addTabContent($lastActiveTabBtn, $lastActiveTabPanel);
